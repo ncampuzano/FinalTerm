92e94ccc98c453d2a10988cd95bc4ec936f26e64
Nicolás Campuzano <nicolas.campuzano@tulpep.com>
2015-06-13 10:06:43 -0500

    Create menu in console

--- FinalProject/Sets.cpp
+++ FinalProject/Sets.cpp
@@ -5,5 +5,17 @@
 using namespace std;
 
 void Sets(){
-
+  write_line(cout);
+  writeln_character_array(cout, "Choose the problem");
+  writeln_character_array(cout, "1. Union");
+  writeln_character_array(cout, "2. Intersection");
+  writeln_character_array(cout, "3. Difference");
+  writeln_character_array(cout, "4. Symmetric difference");
+  writeln_character_array(cout, "5. It belongs");
+  writeln_character_array(cout, "6. Content");
+  option optionNumber = read_option(cin);
+  switch(optionNumber){
+    case 1:Turns();break;
+    case 2:Direction();break;
+  };
 };
